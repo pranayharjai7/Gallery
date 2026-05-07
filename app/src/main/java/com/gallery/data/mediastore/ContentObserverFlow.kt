b68f58efe4888f8a0ed93f1fb64051672e24f8f8
Pranay Harjai <pranayharjai7@gmail.com>
2026-05-07 16:12:07 +0200

    feat(data): add ContentObserver Flow helper

---
+++ app/src/main/java/com/gallery/data/mediastore/ContentObserverFlow.kt
@@ -0,0 +1,21 @@
+package com.gallery.data.mediastore
+
+import android.content.ContentResolver
+import android.database.ContentObserver
+import android.net.Uri
+import android.os.Handler
+import android.os.Looper
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.onStart
+
+fun ContentResolver.observeUri(uri: Uri): Flow<Unit> = callbackFlow {
+    val observer = object : ContentObserver(Handler(Looper.getMainLooper())) {
+        override fun onChange(selfChange: Boolean) {
+            trySend(Unit)
+        }
+    }
+    registerContentObserver(uri, true, observer)
+    awaitClose { unregisterContentObserver(observer) }
+}.onStart { emit(Unit) }
